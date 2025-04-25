7f263d0b19682c28b5802e61ac7d6be44b3059d0
muwomotapiwa <muwomotapiwa@gmail.com>
2025-04-25 14:10:51 +0200

    contact

--- js/contact.js
+++ js/contact.js
@@ -5,8 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    // Simple success message simulation
+    // Simulate form submission success
     form.reset();
     formMessage.classList.remove("hidden");
+
+    setTimeout(() => {
+      formMessage.classList.add("hidden");
+    }, 5000); // Hide message after 5 seconds
   });
 });
