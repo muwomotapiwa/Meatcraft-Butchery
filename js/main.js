0c39d2073312c992191a93b8fea676ad694f2937
muwomotapiwa <muwomotapiwa@gmail.com>
2025-04-25 09:00:17 +0200

    ghjgjh

--- js/main.js
+++ js/main.js
@@ -78,3 +78,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  hamburger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
